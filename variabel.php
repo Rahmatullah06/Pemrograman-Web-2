5a1eaef39b9b2cd3dbb785787d4ed2d244a71b07
Rahmatullah06 <93178321+Rahmatullah06@users.noreply.github.com>
2021-11-03 17:13:59 +0800

    Add files via upload

---
+++ variabel.php
@@ -0,0 +1,10 @@
+<?php
+/** 
+* Ini adalah nilai awal variael
+*/
+$nama = "Rahamatullah";
+$usia = 21;
+echo "<p>Hai, perkenalkan nama saya adalah" ".$nama.", dan saya
+berusia ".$usia." tahun<p>;
+
+?>
